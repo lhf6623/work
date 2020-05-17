b033235c4897f0ef9560724cf4e364cc585a95c7
lhf6623 <1336955219@qq.com>
2020-05-17 11:05:54 +0800

    给傻蛋的更新

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {start} = require('./server')
 const {route} = require('./router')
 const requestHandlers = require('./requestHandlers')
-
+const fool = '傻蛋'
 let handle = {}
 
 handle['/'] = requestHandlers.start
